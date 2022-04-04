3bbb15d2773383419704063ce063b5093ec56195
Olena Kraemer <olenakraem@gmail.com>
2022-04-04 15:20:25 +0200

    1st iteration done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const IdCard = (props) => {
+  return (
+    <div className="profile">
+      <img alt={props.firstName} src={props.picture}></img>
+      <ul>
+        <li>
+          <b>First name:</b> {props.firstName}
+        </li>
+        <li>
+          <b>Last name:</b> {props.lastName}
+        </li>
+        <li>
+          <b>Gender:</b> {props.gender}
+        </li>
+        <li>
+          <b>Height:</b> {props.height}
+        </li>
+        <li>
+          <b>Birth:</b> {props.birth}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const IdCardMain = () => {
+  return (
+    <div>
+      <div>
+        <IdCard
+          lastName="Doe"
+          firstName="John"
+          gender="male"
+          height={178}
+          // birth={new Date('1992-07-14')}
+          picture={'https://randomuser.me/api/portraits/men/44.jpg'}
+        />
+      </div>
+      <div>
+        <IdCard
+          lastName="Delores "
+          firstName="Obrien"
+          gender="female"
+          height={172}
+          // birth={new Date('1988-05-11')}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IdCardMain />
     </div>
   );
 }
